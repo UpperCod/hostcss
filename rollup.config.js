08bb73a8fec3e62f309ec949389677f8982c21cc
Uppercod <mtrujillo@outlook.cl>
2019-05-01 01:22:36 -0400

    update pkg 0.1.0, support for umd and cjs is removed, only modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,15 @@
 import pkg from "./package.json";
-import buble from "rollup-plugin-buble";
-import { terser } from "rollup-plugin-terser";
-import { sizeSnapshot } from "rollup-plugin-size-snapshot";
-
-let plugins = [
-	buble({
-		jsx: "h",
-		objectAssign: "Object.assign"
-	}),
-	terser(),
-	sizeSnapshot()
-];
 
 export default [
 	{
 		input: pkg.source,
 		output: [
 			{
-				file: pkg.main,
-				sourcemap: true,
-				format: "cjs"
-			},
-			{
-				file: pkg.unpkg,
-				sourcemap: true,
-				format: "umd",
-				name: pkg.name,
-				globals: {
-					"@atomico/core": "@atomico/core"
-				}
-			},
-			{
 				file: pkg.module,
 				sourcemap: true,
 				format: "es"
 			}
-		],
-		plugins
+		]
 	},
 	...["atomico", "react", "preact"].map(name => ({
 		input: `src/${name}/index.js`,
@@ -44,20 +17,8 @@ export default [
 			{
 				file: name + ".js",
 				sourcemap: true,
-				format: "cjs"
-			},
-			{
-				file: name + ".umd.js",
-				sourcemap: true,
-				format: "umd",
-				name: "hostcss-" + name
-			},
-			{
-				file: name + ".mjs",
-				sourcemap: true,
 				format: "es"
 			}
-		],
-		plugins
+		]
 	}))
 ];
